3fdbd822a3f55050cb6260e1feba6c0688146043
jjohanj <johanjorritsma@hotmail.com>
2018-02-14 16:08:08 +0100

    add posts timestamps

--- database/migrations/2018_02_14_094736_create_posts_table.php
+++ database/migrations/2018_02_14_094736_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->text('body');
             $table->string('category');
+            $table->timestamps();
         });
     }
 
