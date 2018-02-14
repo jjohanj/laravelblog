c166a25dfdc63ec541437740dda57b9650b5d2d2
jjohanj <johanjorritsma@hotmail.com>
2018-02-14 15:50:07 +0100

    reset

--- database/migrations/2018_02_14_094736_create_posts_table.php
+++ database/migrations/2018_02_14_094736_create_posts_table.php
@@ -13,7 +13,7 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('articles', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
             $table->string('title');
             $table->text('body');
@@ -28,6 +28,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('articles');
     }
 }
