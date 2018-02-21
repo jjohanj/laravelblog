8c70a7641d6082cb72549386bf289b61bfcb5f12
Arend-jan Breukink <awjbreukink@gmail.com>
2018-02-21 13:45:05 +0100

    [W3-006] pivot table

--- database/migrations/2018_02_15_123459_create_category_table.php
+++ database/migrations/2018_02_15_123459_create_category_table.php
@@ -15,10 +15,18 @@ class CreateCategoryTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
         Schema::dropIfExists('table');
+
+        Schema::create('category_post', function (Blueprint $table) {
+            $table->integer('category_id');
+            $table->integer('post_id');
+            $table->primary(['category_id', 'post_id']);
+        });
+        Schema::dropIfExists('table');
+
     }
 
     /**
