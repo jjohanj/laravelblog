9b5cd4ee36907c6535dfd591de4c1f9e0aeaaf68
Julia van Drunen <jdrunen@localhost.localdomain>
2018-02-22 15:34:14 +0100

    idk

--- app/User.php
+++ app/User.php
@@ -32,11 +32,5 @@ class User extends Authenticatable
         return $this->hasMany(Post::class);
     }
 
-    public function publish(Post $post)
-    {
-        $this->posts()->save($post);
-
-        
-
-    }
+   
 }
