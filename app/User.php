f895dcda47ee698eb53a226772d2ac5aeaf5901e
Julia van Drunen <jdrunen@localhost.localdomain>
2018-02-26 16:16:14 +0100

    dummy upgrade view

--- app/User.php
+++ app/User.php
@@ -43,6 +43,10 @@ public function followings()
 {
     return $this->belongsToMany(User::class, 'followers', 'follower_id', 'leader_id')->withTimestamps();
 }
+public function role()
+{
+    
+}
 
-   
+
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('blog_name')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('role')->default('free');
             $table->rememberToken();
             $table->timestamps();
         });
