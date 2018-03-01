2758fb8f6ed3a2a2f8ca8ce801ad39dcf640a3d8
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-01 17:40:57 +0100

    removed input fields and dummy button from downgrade.blade

--- resources/views/downgrade.blade.php
+++ resources/views/downgrade.blade.php
@@ -14,21 +14,11 @@
 <form action="/cancelsubscription" method="POST">
   {{ csrf_field() }}
 
-  <div id="inputfield"></div>
-  Please enter your credentials <br/>
-    <input placeholder="Your name" name="Name" type="text" id="name" ></br>
 
-    payment method: <br/>
-
-    <input placeholder="paypal email" name="paypal" type="text" id="paypal" ></br>
-    <input placeholder="creditcard" name="creditcard" type="text" id="creditcard" ></br>
-    <input type='hidden' value='no' name='agree'><br/>
 
     <button class="btn btn-primary" type="submit">Cancel my subscription!</button></br>
 
 </form>
 
-    <button class="btn btn-primary" type="submit">Yes, cancel my subscription!</button></br>
-
-</form>
+  
 @endsection
