a26f3b934605a7c3c239bf41e1872d5d167ceb28
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-06 15:54:43 +0100

    home layout, edited posts.categories, and posts.index

---
+++ resources/views/layouts/home.blade.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <head>
+        <title>Secure Beyond</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script> window.Laravel = { csrfToken: '{{ csrf_token() }}'} </script>
+
+    <title>Secure Beyond</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                @if(Auth::check())
+                <li class="nav-item">
+                    <a class="nav-link" href="/posts/create">@lang('messages.write')</a>
+                </li>
+                @endif
+
+
+            </ul>
+        </div>
+        <div class="mx-auto order-0">
+            <a class="navbar-brand mx-auto" href="/info">Secure Beyond</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ml-auto">
+                @if(Auth::check())
+                <li class="nav-item">
+                    <a class="nav-link" href="/settings">@lang('messages.settings')</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/logout">@lang('messages.logout')</a>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link" href="/login">@lang('messages.login')</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/register">@lang('messages.register')</a>
+                </li>
+                @endif
+            </ul>
+        </div>
+    </nav>
+
+
+    <div class='container-fluid' width='100%'>
+       <div class="row" >
+          <div class='col-3' style="padding:0">
+            <div class="card">
+              <div class="card-header">
+                <h5 class="mb-0"><a href="/user/{{Auth::user()->name}}" style="text-decoration: none;"> <strong>@lang('messages.greeting') {{Auth::user()->name}}</strong></a></h5>
+              </div>
+              <div class="card-content">
+
+              <a href="/all" class="list-group-item list-group-item-action" style="border-left:0;border-right:0"><strong>@lang('messages.showall')</strong></a>
+              <div style="margin-top:5px;margin-bottom:5px">
+              <form class="form-inline my-2 my-lg-0" action="/?search={{ isset($search) ? $search : '' }}" method="GET">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search for.." aria-label="Search" value="{{ isset($search) ? $search : '' }}">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">@lang('messages.search')</button>
+              </form>
+            </div>
+            </div>
+            </div>
+            <div class="card">
+                <div class="card-header">
+                  <h5 class="mb-0">top users</h5>
+                </div>
+
+                          <div class="list-group" style="border:0">
+                  	@foreach($topusers as $topuser)
+                  	<a href="/user/{{$topuser->name}}" class="list-group-item list-group-item-action" style="border-left:0;border-right:0"><strong>{{$topuser->name}}</strong></a>
+              	    @endforeach
+                  </div>
+
+              </div>
+              <div class="card">
+                <div class="card-header">
+                  <h5 class="mb-0"> @lang('messages.categories') </h5>
+                </div>
+                <div class="card-body">
+              	@foreach ($categories as $category)
+                  <button class="btn btn-success btn-lg btn-block" id="catfilter" onclick="sort('{{ $category->name }}')">{{ $category->name}}</button><br/>
+                @endforeach
+              </div>
+            </div>
+            <div class="card">
+              <div class="card-header">
+               <h5 class="mb-0"> @lang('messages.archive')</h5>
+             </div>
+             <div class="card-body">
+              @foreach($archives as $year => $months)
+                  <h6 class="mb-0">
+                    <a href="?month={{$year}}">
+                      {{ $year }}
+                    </a><br>
+                  </h6>
+              @endforeach
+            </div>
+          </div>
+      </div>
+
+
+
+      <div class ="col">
+        @yield('content')
+      </div>
+
+      <script>
+      function sort(name){
+      $.ajax({
+      		url: '/categories/'+name,
+      		type: "GET", // not POST, laravel won't allow it
+      		success: function(data){
+      			$data = $(data); // the HTML content your controller has produced
+      			$('.col').fadeOut().html($data).fadeIn();
+      			}
+      	});
+      };
+      </script>
+  </body>
+</html>
--- resources/views/layouts/profilepage.blade.php
+++ resources/views/layouts/profilepage.blade.php
@@ -7,60 +7,54 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script> window.Laravel = { csrfToken: '{{ csrf_token() }}'} </script>
 
     </head>
     <body>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+          <div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+              <ul class="navbar-nav mr-auto">
+                  <li class="nav-item active">
+                      <a class="nav-link" href="/">Home</a>
+                  </li>
+                  @if(Auth::check())
+                  <li class="nav-item">
+                      <a class="nav-link" href="/posts/create">@lang('messages.write')</a>
+                  </li>
+                  @endif
 
 
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
-  <a class="navbar-brand" href="/info">Secure Beyond</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-      </li>
-      @if(auth::check())
-      <li class="nav-item">
-        <a class="nav-link" href="/settings">@lang('messages.settings')</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/posts/create">@lang('messages.write')</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/logout">@lang('messages.logout')</a>
-      </li>
-@else
-      <li class="nav-item">
-        <a class="nav-link" href="/login">@lang('messages.login')</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/register"> @lang('messages.register')</a>
-      </li>
-@endif
-
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          languages
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" role="presentation" href="/de">Deutsch</a>
-          <a class="dropdown-item" role="presentation" href="/en">English</a>
+              </ul>
+          </div>
+          <div class="mx-auto order-0">
+              <a class="navbar-brand mx-auto" href="/info">Secure Beyond</a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+                  <span class="navbar-toggler-icon"></span>
+              </button>
+          </div>
+          <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+              <ul class="navbar-nav ml-auto">
+                  @if(Auth::check())
+                  <li class="nav-item">
+                      <a class="nav-link" href="/settings">@lang('messages.settings')</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="/logout">@lang('messages.logout')</a>
+                  </li>
+                  @else
+                  <li class="nav-item">
+                      <a class="nav-link" href="/login">@lang('messages.login')</a>
+                  </li>
+                  <li class="nav-item">
+                      <a class="nav-link" href="/register">@lang('messages.register')</a>
+                  </li>
+                  @endif
+              </ul>
+          </div>
+      </nav>
 
 
-        </div>
-      </li>
-
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
 
 
 
@@ -172,7 +166,20 @@
                  </div>
                </div>
              </div>
-
+<div class="card">
+  <div class="card-header">
+   <h5 class="mb-0">@lang('messages.archives') </h5>
+<div class="card-body">
+@foreach($archives as $year => $months)
+    <h6 class="mb-0">
+      <a href="?month={{$year}}">
+        {{ $year }}
+      </a><br>
+    </h6>
+@endforeach
+</div>
+</div>
+</div>
            </div>
 
 
