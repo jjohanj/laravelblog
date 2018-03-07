ce2ef478c12e741d3e7bd02b5b4e5e58eab980a1
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-07 15:43:31 +0100

    removed bootstrap stuff from stats view, didn't play nice with other scripts

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -2,9 +2,7 @@
 <html>
     <head>
 
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
     <script>
     (function(w,d,s,g,js,fs){
       g=w.gapi||(w.gapi={});g.analytics={q:[],ready:function(f){this.q.push(f);}};
@@ -14,8 +12,9 @@
     }(window,document,'script'));
     </script>
   </head>
-<body style="background-color:#b2b4b7;">
-@include('layouts.navbar')
+<body>
+<a href="/settings"> Back to settings</a>
+<a href="/"> Home</a>
 
   <hr size="10px" noshade>
   <div id="embed-api-auth-container" style="display: none;"></div>
@@ -111,7 +110,7 @@ gapi.analytics.ready(function() {
       'end-date': 'today',
       sort: '-ga:exits',
     },
-    chart: {style="background-color:#b2b4b7;"
+    chart: {
       container: 'chart-3-container',
       type: 'TABLE',
       options: {
