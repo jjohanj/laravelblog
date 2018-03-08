626ac37aff78230bad5469bb510528d748496c8b
Julia van Drunen <jdrunen@localhost.localdomain>
2018-03-08 13:34:53 +0100

    updated the edit page to match new look

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,58 +1,60 @@
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
 
 @section('scripts')
   @include ('layouts.texteditexpand')
 @endsection
 
-@extends ('layout')
+@extends ('layouts.create')
 
 @section ('content')
-  <a href="/">BACK TO HOME PAGE</a><br>
+
 
 @include ('layouts.errors')
 
-<strong>Editing Blog post</strong>
+<div class="container-fluid"style="margin-top:1rem">
+  <div class="card">
+  <div class="card-header">
+    <h3 class="card-title">Edit or delete your blog post</h3>
 
-<div id="form">
-  <div id='form'>
-    <form method="post" action="{{action('PostsController@update', $id)}}" >
-      {{csrf_field()}}
+  </div>
+  <div class="card-body">
 
+  <form method="post" action="{{action('PostsController@update', $id)}}" >
+  {{ csrf_field() }}
       <input name="_method" type="hidden" value="PATCH">
 
-      <div id="inputfield"></div>
+  <div class="form-group">
+    <input type="hidden" value="{{csrf_token()}}" name="_token">
 
-        <input type="hidden" value="{{csrf_token()}}" name="_token" />
+    <label for="title">Title</label>
+    <input type="text" class="form-control" name="title" value='{{$post->title}}'>
+  </div>
 
-        <label for="title">Title:</label>
-        <input type="text" class="form-control" name="title" value={{$post->title}} /></br>
-
-      </div>
 
       <div class="form-group">
 
-        <label for="description">Content:</label>
-        <textarea cols="5" rows="5" class="form-control" name="body">{{$post->body}}</textarea>
+    <label for="description">Content</label>
+      <textarea cols="5" rows="5" class="form-control" name="body">{{$post->body}}</textarea>
+ </div>
+<div class="form-group">
 
-      </div>
+        <input type='hidden' value='no' name='disable_comments'><br/>
+        <input type='checkbox' value='yes' name='disable_comments'> Disable comments <br/>
 
-      <input type='hidden' value='no' name='disable_comments'><br/>
-      <input type='checkbox' value='yes' name='disable_comments'> Disable comments <br/>
-
-    <button class="btn btn-primary" type="submit">Update</button></br>
-
+</div>
+<button class="btn btn-success" type="submit">Update</button></br>
 </form>
-  <form method="post" action="{{action('PostsController@destroy', $id)}}">
-    {{csrf_field()}}
+<div class="container-fluid">
+<form method="post" action="{{action('PostsController@destroy', $id)}}" style="float:right">
+  {{csrf_field()}}
 
-    <input name="_method" type="hidden" value="DELETE">
+  <input name="_method" type="hidden" value="DELETE">
 
-    <button class='btn btn-danger' type="submit" >Delete</button>
-  </form>
-
+  <button class='btn btn-danger' type="submit" >Delete</button>
+</form>
+</div>
+  </div>
+</div>
 </div>
 
 
